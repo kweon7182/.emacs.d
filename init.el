f345ea9efb0d3911230d6b0f375a3321383947b7
Hyuk Jun Kweon <kweon7182@gmail.com>
2016-02-28 19:19:37 +0900

    update theme

--- init.el
+++ init.el
@@ -11,6 +11,12 @@
 (setq settings-dir (locate-user-emacs-file "settings"))
 (add-to-list 'load-path settings-dir)
 
+;; theme files
+(setq theme-dir (locate-user-emacs-file "theme"))
+(add-to-list 'custom-theme-load-path theme-dir)
+(load-theme 'light)
+
+
 ;; from emacs
 (require 'cl)
 
