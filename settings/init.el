bcfc90ef83664f78015dbca08c61236523d0fe30
Hyuk Jun Kweon <kweon7182@gmail.com>
2016-02-28 15:49:22 +0900

    add sage-mode

--- settings/init.el
+++ settings/init.el
@@ -6,14 +6,8 @@
 ;; for sage
 (require 'sage)
 (setq sage-command "sage")
-;; If you want sage-view to typeset all your output and have plot()
-;; commands inline, uncomment the following line and configure sage-view:
-;; (require 'sage-view "sage-view")
-;; (add-hook 'sage-startup-after-prompt-hook 'sage-view)
-;; You can use commands like
-;; (add-hook 'sage-startup-after-prompt-hook 'sage-view-disable-inline-output)
-;; (add-hook 'sage-startup-after-prompt-hook 'sage-view-disable-inline-plots)
-;; to enable some combination of features.  Using sage-view requires a
-;; working LaTeX installation with the preview package.
+(if (not (fboundp 'sage-mode))
+    (sage-update-autoloads))
+
 
 (provide 'init)
