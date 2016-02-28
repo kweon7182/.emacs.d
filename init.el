405fd9c6537d37f53d2f764aac25aa9cf9c7ac38
Hyuk Jun Kweon <kweon7182@gmail.com>
2016-02-28 14:37:39 +0900

    add sage-mode from Mercurial repository

--- init.el
+++ init.el
@@ -11,56 +11,22 @@
 (setq settings-dir (locate-user-emacs-file "settings"))
 (add-to-list 'load-path settings-dir)
 
-;; require from emacs
-(require 'cl)
 
-;; require from site-lisp
-(require 'restart-emacs)
-
-;; require from settings
-(require 'functions)
-(require 'init)
-
-
-
-
-
-
-
-
-
-;; Automatic package installation
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(package-initialize)
-
-(defvar initial-packages '(auctex 
-     visual-regexp
-     markdown-mode
-     fill-column-indicator
-     flycheck
-     flycheck-pos-tip
-     flycheck-clojure
-     flx
-     f
-     magit)
+;; global variables
+(defvar initial-packages
+  '(auctex magit)
   "A list of packages to be installed")
 
-(defvar is_first_run 
-  (loop for p in initial-packages
-     when (not (package-installed-p p)) do (return nil)
-     finally (return t)))
 
-(if is_first_run
-  (message "Start refreshing packages.")
-  (package-refresh-contents)
-  
-  (dolist (p initial-packages)
-    (when (not (package-installed-p p))
-      (package-install p)))
-  (bypass-confirmation #'restart-emacs))
+;; from emacs
+(require 'cl)
 
+;; from site-lisp
+(require 'restart-emacs)
+
+;; from settings
+(require 'functions)
+(require 'init)
+(require 'key-bindings)
+(require 'init-packages)
 
