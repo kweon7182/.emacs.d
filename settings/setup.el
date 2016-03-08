ffcab5ec7092805211f174c57ab3baed23e97692
Hyuk Jun Kweon <kweon7182@gmail.com>
2016-03-08 11:19:11 +0900

    setting for cuda

--- settings/setup.el
+++ settings/setup.el
@@ -3,12 +3,12 @@
 
 ;; for c-mode
 (setq c-default-style "linux" c-basic-offset 4)
+(add-to-list 'auto-mode-alist '("\\.cu$" . c++-mode))
 
 ;; for sage
 (require 'sage)
 (setq sage-command "sage")
 (if (not (fboundp 'sage-mode))
     (sage-update-autoloads))
-
 
 (provide 'setup)
