bdf1bffb51a6f802d7a03303406db0dfa5e94433
Hyuk Jun Kweon <Hyuk Jun Kweon>
2016-03-12 13:25:11 +0900

    tex setting for Windows

--- init.el
+++ init.el
@@ -29,7 +29,16 @@
 (require 'setup)
 (require 'key-bindings)
 
-
 ;; load theme
 (load-theme 'organic-green)
 
+;; for windows
+(if (eq system-type 'windows-nt)
+    (setq TeX-view-program-list
+	  '(("Sumatra PDF" ("\"C:/Program Files/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
+			    (mode-io-correlate " -forward-search %b %n -inverse-search \"C:/Program Files/emacs-24.5/bin/emacsclientw --no-wait +%%l \\\"%%f\\\"\" ") " %o")))))
+
+(with-eval-after-load 'tex
+  (when (eq system-type 'windows-nt)
+    (assq-delete-all 'output-pdf TeX-view-program-selection)
+    (add-to-list 'TeX-view-program-selection '(output-pdf "Sumatra PDF"))))
